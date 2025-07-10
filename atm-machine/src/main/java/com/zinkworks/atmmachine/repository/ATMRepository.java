3f9e9f3ec020f19d3414a620e412c2b800f3868b
Manish Singh <30594311+singh-man@users.noreply.github.com>
2025-07-10 22:48:53 +0200

    Update ATMRepository.java

--- atm-machine/src/main/java/com/zinkworks/atmmachine/repository/ATMRepository.java
+++ atm-machine/src/main/java/com/zinkworks/atmmachine/repository/ATMRepository.java
@@ -6,9 +6,7 @@ import org.springframework.stereotype.Repository;
 import com.zinkworks.atmmachine.entity.ATM;
 
 /**
- * 
  * @author Manish.Singh
- *
  */
 @Repository
 public interface ATMRepository extends JpaRepository<ATM, Long> {
